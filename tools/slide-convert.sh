1d293a9eca37db4faa070f7db129f32e7bfea102
William Floyd <william.png2000@gmail.com>
2020-11-10 02:27:35 -0600

    slide-convert: Convert straight from pptx.

--- tools/slide-convert.sh
+++ tools/slide-convert.sh
@@ -1,18 +1,52 @@
 #!/bin/bash
-# ./slide-convert.sh <DIR_NAME>
+# ./slide-convert.sh <FILE.pptx>
 # Use this script to ingest HTML/image files of powerpoint presentations exported from Impress.
 
-prefix='img'
-escaped="$(sed 's/ /\\\\ /g' <<< "${1}")"
+if [ "${#}" -lt 1 ]; then
+    echo 'Provide some input!'
+    exit
+fi
 
-pushd "${1}"
+name="$(sed -e 's/\.[^\.]*$//' <<<"${1}")"
+
+if [ -d "${name}" ]; then
+    rm -r "${name}"
+fi
+
+unoconv -vvv --doctype=presentation --format=html -e "PublishMode"=0 -e 'IsExportContentsPage=false' -e 'HiddenSlides=true' -e 'Width=800' -e 'Format=2' -e 'UseButtonSet=-1' --output="${name}/" "${1}"
+
+prefix='img'
+format='png'
+escaped="$(sed -e 's/ /\\\\ /g' -e 's/&/\\&/g' <<<"${name}")"
+
+pushd "${name}"
 
 n="$(find . -type f | grep "${prefix}[0-9]*\.html" | wc -l)"
 
-for i in $(seq 0 "$[n-1]"); do
+if [ -e '.html' ]; then
+    mv '.html' 'img0.html'
+fi
+
+for i in $(seq 0 "$((n - 1))"); do
     source="${prefix}${i}.html"
-    echo "# $(pcregrep -Mo '<title>(.|\n)*?</title>' < "${source}" | tr -d '\n' | sed 's|<title>\(.*\)</title>|\1|')"
-    html2md -i  < "${source}" | sed "s|\(img[0-9]*.png\)|${escaped}/\1|" | grep -v '\.html)'
-done > "../${1}.md"
+    #echo "# $(pcregrep -Mo '<title>(.|\n)*?</title>' <"${source}" | tr -d '\n' | sed 's|<title>\(.*\)</title>|\1|')"
+
+    output="$(html2md -i <"text${i}.html" | sed "s|\(${prefix}[0-9]*.${format}\)|${escaped}/\1|" | grep -v '\.html)' | sed -e "s|^# |# Slide ${i} - |")"
+    if ! grep -qE '^# ' <<<"${output}"; then
+        echo "# Slide $((i + 1))"
+    fi
+    echo "${output}"
+    html2md -i <"${source}" | sed "s|\(${prefix}[0-9]*.${format}\)|${escaped}/\1|" | grep -v '\.html)'
+done >"../${1}.md"
+
+rm *.html
+
+zopflipng --prefix *.png
+
+for file in zopfli_img*; do
+    mv "${file}" "${file#zopfli_}"
+done
 
 popd
+
+exit
