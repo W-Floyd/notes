0aba9232eea7997ee339eaa3441260a95198e71b
William Floyd <william.png2000@gmail.com>
2021-02-04 00:46:56 -0600

    image-localizer: Add a quick script to pull all images from web links and store locally. Allows offline builds.

---
+++ tools/image-localizer.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd './source/' || {
+    echo 'Not in the right directory, run from root of notes folder'
+    exit 1
+}
+
+__img_dir='./img'
+
+grep -rEl '!\[[^]]*\]\([http|https][^)]*\)' | while read -r __file; do
+    grep -hoE '!\[[^]]*\]\([http|https][^)]*\)' <"${__file}" | sed -e 's/.*(\(.*\))/\1/' | while read -r __link; do
+        __hash="$(sha1sum <<<"${__link}" | sed 's/ .*//')"
+        __output="${__img_dir}/${__hash}.${__link/*./}"
+        sed -e "s|${__link}|!imgdir/${__hash}.${__link/*./}|g" -i "${__file}"
+        curl "${__link}" -o "${__output}"
+    done
+done
+
+exit
